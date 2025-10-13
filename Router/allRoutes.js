218b0eda6877464773f08bb4aa9967f6d48bebf0
nelly34800 <nellyboussekane@free.fr>
2025-10-13 19:24:58 +0200

    ajout page la carte et interface utilisateur (mon compte, inscription, connexion, changement de mot de passe)

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,17 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
-  new Route("/galerie", "La galerie", "/pages/galerie.html"),
+  new Route("/galerie", "Galerie", "/pages/galerie.html"),
+  new Route("/carte", "La carte", "/pages/carte.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signout", "Déconnexion", "/pages/auth/signout.html"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html"),
+  new Route(
+    "/editPassword",
+    "Changer mot de passe",
+    "/pages/auth/editPassword.html"
+  ),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
