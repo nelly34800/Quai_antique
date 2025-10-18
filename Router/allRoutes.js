4f3e463cc68b2d77b97f53027f3d81c705f6445f
nelly34800 <nellyboussekane@free.fr>
2025-10-18 08:53:34 +0200

    validation champs inscription et getion de la connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,15 +4,17 @@ export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "Galerie", "/pages/galerie.html"),
   new Route("/carte", "La carte", "/pages/carte.html"),
-  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html", "js/auth/signin.js"),
   new Route("/signout", "Déconnexion", "/pages/auth/signout.html"),
-  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html", "js/auth/signup.js"),
   new Route("/account", "Mon compte", "/pages/auth/account.html"),
   new Route(
     "/editPassword",
     "Changer mot de passe",
     "/pages/auth/editPassword.html"
   ),
+  new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
+  new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
